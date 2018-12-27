503331d07d355b8b404ad2ed1c53e777a43f0424
高建文 <cmdgjw@hotmail.com>
2018-12-27 21:30:32 +0800

    增加日志输出

--- lumberjack-demo/main.go
+++ lumberjack-demo/main.go
@@ -12,13 +12,13 @@ var (
 
 func main() {
 	logger = &lumberjack.Logger{
-		Filename:   "./logs/demo.log",
-		MaxSize:    1,
-		MaxBackups: 60,
-		MaxAge:     30,
-		Compress:   true,
+		Filename:   "./logs/demo.log", // 默认文件
+		MaxSize:    1,                 // 最大文件尺寸（超过备份）
+		MaxBackups: 60,                // 最大备份数量
+		MaxAge:     30,                // 最大备份保存时长
+		Compress:   true,              // 是否压缩
 	}
-	log.SetOutput(logger)
-	log.Println("fsdf")
+	log.SetOutput(logger) // 接管系统日志输出
+	log.Println("fsdf")   // 测试输出
 
 }
